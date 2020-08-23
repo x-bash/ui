cf3591c2d1f9e9d71a944f65797953211d86708e
edwinjhlee <edwin.jh.lee@gmail.com>
2020-08-23 23:24:26 +0800

    Fix some warning.

--- test.param/exe.sh
+++ test.param/exe.sh
@@ -1,1 +1,7 @@
 bash unitest/param.test.1 --org hi  --access private --sort time,repo --show repo,time
+
+
+org=hi access=private bash test.param/param.test.1  --sort time,repo --show repo,time
+
+org=hi sort=time,repo access=private bash test.param/param.test.1 --show repo,time
+
