acde98364280050e7cc48393e2f7705f3796cc50
edwinjhlee <edwin.jh.lee@gmail.com>
2020-08-23 23:33:22 +0800

    Now param support argENV argenv arg env ENV

--- test.param/exe.sh
+++ test.param/exe.sh
@@ -1,7 +1,10 @@
-bash unitest/param.test.1 --org hi  --access private --sort time,repo --show repo,time
+bash test.param/param.test.1 --org hi  --access private --sort time,repo --show repo,time
 
 
 org=hi access=private bash test.param/param.test.1  --sort time,repo --show repo,time
 
 org=hi sort=time,repo access=private bash test.param/param.test.1 --show repo,time
 
+org=hi ACCESS=private bash test.param/param.test.1 --sort time,repo --show repo,time
+
+org=hi sort=time,repo ACCESS=private bash test.param/param.test.1 --show repo,time
