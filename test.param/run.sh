f1cbb973e9afcabee9caa9c0cb9cdba26d901bc3
edwinjhlee <edwin.jh.lee@gmail.com>
2020-11-25 20:33:11 +0800

    stash

--- test.param/run.sh
+++ test.param/run.sh
@@ -11,4 +11,4 @@ org=hi ACCESS=private bash test.param/param.test.1 --sort time,repo --show repo,
 org=hi sort=time,repo ACCESS=private bash test.param/param.test.1 --show repo,time
 
 # show help document
-bash test.param/param.test.1 --org hi  --access private --sort time,repo --show repo,time 12  2 -h
+bash test.param/param.test.1 --org hi --access private --sort time,repo --show repo,time 12 2 -h
