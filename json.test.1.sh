d4f7c6a77c18d96b4ac2f2a647a39ccc3d05e781
edwinjhlee <edwin.jh.lee@gmail.com>
2021-01-24 12:14:33 +0800

    rm json files

--- json.test.1.sh
+++
@@ -1,4 +0,0 @@
-time jq .[].testers[].received_events_url <json-data/b.json
-time json.extract \* testers \* received_events_url <json-data/b.json
-time bash jp<json-data/b.json | grep \"received_events_url\"\]
-
