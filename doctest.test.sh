586dff9200fa051fa0d3e80bd346573a8661f4b4
edwinjhlee <edwin.jh.lee@gmail.com>
2021-01-31 22:16:00 +0800

    stash

---
+++ doctest.test.sh
@@ -0,0 +1,2 @@
+docker run -it -v `pwd`:/pd debian sh -c "X_BASH_SRC_SHELL=sh; . /pd/doctest; doctest /pd/assert"
+docker run -it -v `pwd`:/pd alpine sh -c "X_BASH_SRC_SHELL=sh; . /pd/doctest; doctest /pd/assert"
