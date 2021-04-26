2b02c2c1841830c6a46d960e877b642862a102d4
edwinjhlee <edwin.jh.lee@gmail.com>
2021-04-27 00:00:40 +0800

    ...

--- test.sh
+++ test.sh
@@ -2,6 +2,22 @@
 
 . ./v0
 
-ui_check_cmd "Step-1:  Running Sleep 3"     "sleep 5"
-ui_check_cmd "Step-2:  Running Sleep 3"     "sleep 1; false"
-ui_check_cmd "Step-3:  Running Sleep 3"     "sleep 2; true"
+# ui_check_cmd "Step-1:  Running Sleep 3"     "sleep 5"
+# ui_check_cmd "Step-2:  Running Sleep 3"     "sleep 1; false"
+# ui_check_cmd "Step-3:  Running Sleep 3"     "sleep 2; true"
+
+for i in `seq 0 10 100`; do 
+    if [ $i -eq 60 ]; then
+        echo -$i
+    else
+        echo $i
+    fi
+    sleep 0.2s; 
+done | ui_progressbar
+
+
+for i in `seq 0 10 100`; do 
+    echo $i
+    sleep 0.2s; 
+done | ui_progressbar
+
