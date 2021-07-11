46c82a68f3ad70b885e9d8ab1703d5aad4cce274
edwinjhlee <edwin.jh.lee@gmail.com>
2021-07-11 22:24:03 +0800

    ...

--- test.sh
+++ test.sh
@@ -2,24 +2,24 @@
 
 . ./v0
 
-# ui_check_cmd "Step-1:  Running Sleep 3"     "sleep 5"
-# ui_check_cmd "Step-2:  Running Sleep 3"     "sleep 1; false"
-# ui_check_cmd "Step-3:  Running Sleep 3"     "sleep 2; true"
+ui_check_cmd "Step-1:  Running Sleep 3"     "sleep 5"
+ui_check_cmd "Step-2:  Running Sleep 3"     "sleep 1; false"
+ui_check_cmd "Step-3:  Running Sleep 3"     "sleep 2; true"
 
-# for i in $(seq 0 10 100); do 
-#     if [ $i -eq 60 ]; then
-#         echo -$i
-#     else
-#         echo $i
-#     fi
-#     sleep 0.2s; 
-# done | ui progressbar
+for i in $(seq 0 10 100); do 
+    if [ $i -eq 60 ]; then
+        echo -$i
+    else
+        echo $i
+    fi
+    sleep 0.2s; 
+done | ui progressbar
 
 
-# for i in $(seq 0 10 100); do 
-#     echo $i
-#     sleep 0.2s; 
-# done | ui progressbar
+for i in $(seq 0 10 100); do 
+    echo $i
+    sleep 0.2s; 
+done | ui progressbar
 
 item(){
     local id=${1:?ID}
